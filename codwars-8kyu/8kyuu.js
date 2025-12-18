c3a2aefd3e4b6293ce83f9403fc83f890da231bb
AbdujabborovOrifjon <abdujabborovorifjon46@gmail.com>
2025-12-18 20:30:19 +0500

    change

---
+++ codwars-8kyu/8kyuu.js
@@ -0,0 +1,43 @@
+// function arrayPlusArray(arr1, arr2) {
+//   return arr1.concat(arr2)
+// }
+
+// ==============================================================================================
+
+// 1 kunnnnn
+
+// 1 MASALA 
+// linki : https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+// JAVOBI
+// let numbers = [1, 2, ,3 ,4 ,5 ,6, 7, 8]
+// function sum(numbers) {
+//   let sum = 0
+//    numbers.forEach((value)=>{
+//   sum +=  value 
+//   })
+//   return sum
+// }
+
+// console.log(sum(numbers));
+
+
+
+// ============================================
+
+// 2 MASALA
+
+// LINKI https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+
+// JAVOBI 
+
+// function positiveSum(arr) {
+//   let sum = 0
+//  arr.forEach((value)=>{
+//     if(value > 0) sum += value
+    
+//   })
+//   return sum
+// }
+// console.log(positiveSum());
+
+// =======================================================================================================
