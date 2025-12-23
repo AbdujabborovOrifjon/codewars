ab116f1a3257a531e8bd53a52dfcaae2e89b76ab
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2025-12-23 10:17:16 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -138,3 +138,23 @@
 // return res.split('').join("***")
 // }
 
+
+// ================================================================================
+
+
+// 8 MASALAAAAAAA
+
+// LINKI :https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+// NOMIIII : Basic Mathematical Operations
+
+// JAVOBIIIII :
+// function basicOp(operation, value1, value2){
+// if(operation === "+"){
+//   return value1 + value2
+// }else if(operation === "-"){
+//   return value1 - value2;
+// }else if(operation === "*"){
+//   return value1 * value2
+// }else if(operation === "/" ){
+//   return value1 / value2
+// }}
