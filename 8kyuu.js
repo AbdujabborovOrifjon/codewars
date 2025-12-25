361e5b005a10741c3a89e83b612b7f118ac24f27
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2025-12-25 20:32:53 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -209,3 +209,24 @@
 
 // // JAVOBIII:
 // const  stringToArray = (string)=> string.split(" ")
+
+
+// ===========================================================================================================
+
+// 12 MAASAALAAAAAA
+
+// LINKI : https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+// NOMI : A Needle in the Haystack
+
+// Jvobi :
+// function findNeedle(haystack) {
+//   for (let i = 0; i < haystack.length; i++) {
+//     if (haystack[i] === "needle") {
+//       return "found the needle at position " + i;
+//     }
+//   }
+// }
+
+
+// -------------------------------------------------------------------------
+
