db568fbd8f2bf3ddba83cd5f0d560e15a22569b0
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2025-12-28 14:25:28 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -282,3 +282,13 @@
 
 
 // ====================================================================================
+
+// 17 masalaaaaaa
+
+// Nomiii :Returning Strings
+// linki : https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+// javobiii :
+// function greet(name){
+//     return `Hello, ${name} how are you doing today?`;
+// }
