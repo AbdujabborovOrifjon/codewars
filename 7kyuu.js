380c1c16c312319a1b4af5766348080a5193fec4
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-02 22:39:09 +0500

    change

---
+++ 7kyuu.js
@@ -0,0 +1,23 @@
+    // Codewars  7 kyuu  masalalari 
+
+
+// ================================================================================================
+// 1 Masaalaaaaaaa
+
+// CRUD   ->  READ vazifasi  
+
+// linki : https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+
+// Nomiii  :  Do'stmi yoki dushmanmi?
+
+// Javobiii : 
+
+// function friend(friends){
+// return   friends.filter((n)=>{
+//     if(n.length === 4) return n
+    
+//   })
+// }
+
+
+// ===========================================================================================================
