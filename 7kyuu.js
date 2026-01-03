9cbf9bd3a7211e824a416ba6ec8ae613b3d6b359
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-03 22:20:21 +0500

    change

--- 7kyuu.js
+++ 7kyuu.js
@@ -20,4 +20,17 @@
 // }
 
 
-// ===========================================================================================================
+// =============================================================================================================
+
+// 2 Masaalaaaa 
+// CRUD -> READ  vazifasi 
+
+
+// linki :  https://www.codewars.com/kata/52705ed65de62b733f000064/train/javascript
+
+// Nomii : Saralangan obyektlar ro'yxatini qaytaring
+
+// Javobiii :   
+// function sortList (sortBy, list) {
+//   return  list.sort((a, b)=> b[sortBy] - a[sortBy])
+// }
