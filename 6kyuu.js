01b2d53b669595382f85fce7aecba59f35fea8c1
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-05 15:49:01 +0500

    change

---
+++ 6kyuu.js
@@ -0,0 +1,13 @@
+    // Codewars  6 kyuu  masalalari 
+
+
+    // 1 masaalaaa
+
+    // linki : https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
+
+    // Name :Bitlarni hisoblash
+
+    // javobii : 
+    // : function countBits(n) {
+//     return n.toString(2).split("").filter((bit)=>bit === "1").length
+// }
