3cc88729878e7cc759d1820468fa0b5a49eb0b45
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-05 15:51:35 +0500

    change

--- 6kyuu.js
+++ 6kyuu.js
@@ -1,6 +1,8 @@
     // Codewars  6 kyuu  masalalari 
 
 
+// ======================================================================================
+
     // 1 masaalaaa
 
     // linki : https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
@@ -11,3 +13,5 @@
     // : function countBits(n) {
 //     return n.toString(2).split("").filter((bit)=>bit === "1").length
 // }
+
+// =================================================================================
