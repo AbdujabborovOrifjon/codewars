f12cfc1c3316114b1ab6a88c008f6502e247d8d2
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-07 15:49:06 +0500

    change

--- 7kyuu.js
+++ 7kyuu.js
@@ -52,3 +52,24 @@
 
 
 // ===============================================================================================================
+
+// 4 Masaalaaaaa 
+
+// LINKIIII : https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+
+// Namee : Make a function that does arithmetic!
+
+// Javobuiiii :
+// function arithmetic(a, b, operator){
+//   if(operator === "add"){
+//     return a + b
+//   }else if(operator === "subtract"){
+//     return a - b
+//   }else if (operator === "multiply"){
+//     return a * b
+//   }else if (operator === "divide"){
+//      return a / b
+//   }
+// }
+
+// ===============================================================================================================================
