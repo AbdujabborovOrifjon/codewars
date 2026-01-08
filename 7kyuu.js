3efdd0d5d7ffbdb23b765d44678c2c0a77417f16
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-08 10:33:53 +0500

    change

--- 7kyuu.js
+++ 7kyuu.js
@@ -96,7 +96,7 @@
 
 // 6 Masaallaa
 
-// nanme : Ok funksiyalari bilan saralash
+// Namee : Ok funksiyalari bilan saralash
 
 // linki : https://www.codewars.com/kata/559f3e20f4f29869cf0000ea/train/javascript
 
