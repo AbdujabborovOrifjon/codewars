ad21e8fac0163e9f5a755fbaf59965d0591a3682
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-15 20:50:20 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -433,3 +433,23 @@
 // };
 
 // =============================================================================================================
+
+// 27 Masalaaaaaaaa 
+
+// Nameee : Calculate BMI
+
+// lINKIII : https://www.codewars.com/kata/57a429e253ba3381850000fb/train/javascript
+
+// Javobiiii : 
+// function bmi(weight, height) {
+//   let bmi = weight / (height * height)
+//   if(bmi <= 18.5){
+//     return "Underweight"
+//   }else if(bmi <= 25.0){
+//     return "Normal"
+//   }else if(bmi <= 30.0){
+//     return "Overweight"
+//   }else {
+//     return "Obese"
+//   }
+// }
