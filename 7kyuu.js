6a26991081c7019377d505d32ad8689ff4ea027b
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-19 10:35:24 +0500

    change

--- 7kyuu.js
+++ 7kyuu.js
@@ -193,3 +193,12 @@
 // }
 
 // ======================================================================================================================================
+
+// 14 Masaalaaaaa
+
+// Nameeeeeeeee : Shortest Word
+
+// Linkiiii : https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
+
+// Javoobiiiii : let findShort = (s)=> Math.min(...s.split(" ").map((w)=>w.length))
+
