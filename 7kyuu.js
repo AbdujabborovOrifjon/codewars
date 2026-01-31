e081f755f668b70104f61dfe99a1cbc51f64a586
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-01-31 21:38:56 +0500

    change

--- 7kyuu.js
+++ 7kyuu.js
@@ -283,3 +283,5 @@
 // Linkii : https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
 
 // Javobiii : let  sumTwoSmallestNumbers = (numbers)=>numbers.sort((a, b) => a - b).slice(0, 2) .reduce((a, b) => a + b);
+
+// ============================================================================================================
