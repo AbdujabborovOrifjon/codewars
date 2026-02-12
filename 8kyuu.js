7992708f2561cb46d30f7981bd030d623e04bcef
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-02-12 22:00:54 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -623,3 +623,22 @@
 // Javobii : let past = (h, m, s)=> h * 3600000 + m * 60000 + s * 1000;
 
 // ==========================================================================================
+
+// 40 Masaalaaaa
+
+// Nameee: Classy Classes
+
+// Linkiiii : https://www.codewars.com/kata/55a144eff5124e546400005a/train/javascript
+
+
+// Javobiiiii : 
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   get info() {
+//     return `${this.name}s age is ${this.age}`;
+//   }
+// }
