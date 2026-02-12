4a42efc1905e2691db51871d2cf87ba8558e3b46
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-02-12 21:59:26 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -621,3 +621,5 @@
 // Linkii : https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
 
 // Javobii : let past = (h, m, s)=> h * 3600000 + m * 60000 + s * 1000;
+
+// ==========================================================================================
