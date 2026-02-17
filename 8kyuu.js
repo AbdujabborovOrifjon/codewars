111560bdbdddf4fae70bc2c8f0983340ab86a6f5
AbdujabborovOrifjon <abdujabborovorifjon0@gmail.com>
2026-02-17 15:06:37 +0500

    change

--- 8kyuu.js
+++ 8kyuu.js
@@ -645,3 +645,21 @@
 
 
 // ===================================================================================================
+
+// 41 Masaalaaaaa
+
+// Nameeee : Refactored Greeting
+
+// Linkiiiiiii : https://www.codewars.com/kata/5121303128ef4b495f000001/train/javascript
+
+// Javoooobiiiiiiiiiiiiiiiiiii : 
+
+// class Person{
+//   constructor(name){
+//     this.name = name
+//   }
+//   greet(yourName) {
+//   return `Hello ${yourName}, my name is ${this.name}`;
+// }
+// }
+
